f559bb40f47756a8bd4673a14521696d931562ff
bozya02 <bozinandrey1@yandex.ru>
2022-10-08 14:39:06 +0300

    Add project files.

---
+++ Lopushok/LopushokWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lopushok
+{
+    /// <summary>
+    /// Interaction logic for LopushokWindow.xaml
+    /// </summary>
+    public partial class LopushokWindow : Window
+    {
+        public string pageTitle { get; set; }
+
+        public LopushokWindow()
+        {
+            InitializeComponent();
+
+            frame.Navigated += Frame_Navigated;
+            //frame.NavigationService.Navigate(new AuthorizationPage());
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            var pageContent = frame.Content;
+
+            pageTitle = (pageContent as Page).Title;
+
+            //spButtons.Visibility = pageContent is AuthorizationPage ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.CanGoBack)
+                frame.GoBack();
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.CanGoForward)
+                frame.GoForward();
+        }
+    }
+}
