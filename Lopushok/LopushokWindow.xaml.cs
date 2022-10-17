3f98e5c8c1f89963e11a022dc40a88d7668de988
Михаил Мясников <gutufik@mail.ru>
2022-10-17 15:13:03 +0300

    update data base

--- Lopushok/LopushokWindow.xaml.cs
+++ Lopushok/LopushokWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Lopushok.DB;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,9 @@ namespace Lopushok
         public List<Product> Products { get; set; }
         public string Name { get; set; }
 
+        public int PageNumber { get; set; } = 1; 
+        public string PageNumberText { get; set; }
+
         public List<string> SortTypes { get; set; }
         public List<ProductType> ProductTypes  { get; set; }
 
@@ -41,7 +45,16 @@ namespace Lopushok
                 "Минимальная стоимость по убыванию", "Минимальная стоимость по возрастанию"
             };
             ProductTypes = DataAccess.GetProductTypes();
+
+            ProductsList.ItemsSource = Products.Skip((PageNumber - 1) * 20).Take(20);
+
+            ProductsList.Items.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
             ProductTypes.Insert(0, new ProductType { Name = "Все типы" });
+
+            PageNumberText = string.Join<string>(" ", Enumerable.Range(1, (int)(Products.Count / 20)).Select(x => x.ToString()));
+
+            var t = new StackPanel { Children = { new Button { Content = "1",  } } };
+
             DataContext = this;
         }
 
@@ -52,12 +65,51 @@ namespace Lopushok
 
         private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+            if (cbSort.SelectedItem.ToString() == "Номер цеха по возрастанию")
+                ProductsList.Items.SortDescriptions[0] = new SortDescription("WorkshopId", ListSortDirection.Ascending);
+            else if (cbSort.SelectedItem.ToString() == "Номер цеха по убыванию")
+                ProductsList.Items.SortDescriptions[0] = new SortDescription("WorkshopId", ListSortDirection.Descending);
+            else if (cbSort.SelectedItem.ToString() == "Минимальная стоимость по возрастанию")
+                ProductsList.Items.SortDescriptions[0] = new SortDescription("MinPrice", ListSortDirection.Ascending);
+            else if (cbSort.SelectedItem.ToString() == "Минимальная стоимость по убыванию")
+                ProductsList.Items.SortDescriptions[0] = new SortDescription("MinPrice", ListSortDirection.Descending);
+            else if (cbSort.SelectedItem.ToString() == "Наименование по возрастанию")
+                ProductsList.Items.SortDescriptions[0] = new SortDescription("Name", ListSortDirection.Ascending);
+            else
+                ProductsList.Items.SortDescriptions[0] = new SortDescription("Name", ListSortDirection.Descending);
         }
 
         private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void PreviousPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PageNumber > 1)
+            {
+                PageNumber--;
+                ProductsList.ItemsSource = Products.Skip((PageNumber - 1) * 20).Take(20);
+                PageNumberTextBlock.Text = PageNumber.ToString();
+            }
+
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PageNumber < Products.Count / 20)
+            {
+                PageNumber++;
+                ProductsList.ItemsSource = Products.Skip((PageNumber - 1) * 20).Take(20);
+                PageNumberTextBlock.Text = PageNumber.ToString();
+            }
+        }
+
+        private void AddProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Windows.AddProductWindow();
+            window.ShowDialog();
+            
         }
     }
 }
