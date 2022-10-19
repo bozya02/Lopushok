250136924e1d4a09e1d3aaeb66c585bbd7947dd0
misha <misha@misha.ru>
2022-10-19 20:02:51 +0300

    add colorful selection

--- Lopushok/DB/ProductPart.cs
+++ Lopushok/DB/ProductPart.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Lopushok.DB
 {
     public partial class Product
     {
         public string Materials => string.Join(", ", ProductMaterials.Select(x => x.Material.Name));
+
+        public SolidColorBrush Color => ProductSales.Any(x => x.Sale.Date.Value.Month == DateTime.Today.Month) ?
+             new SolidColorBrush(Colors.Transparent) : new SolidColorBrush(Colors.LightCoral);
     }
 }
